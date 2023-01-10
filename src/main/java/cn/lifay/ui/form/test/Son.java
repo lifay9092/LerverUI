c0cb12c93c37923876d42a3253a6a456db8fd106
lifay9092 <422980644@qq.com>
2023-01-10 21:20:05 +0800

    获取泛型DEMO

---
+++ src/main/java/cn/lifay/ui/form/test/Son.java
@@ -0,0 +1,18 @@
+package cn.lifay.ui.form.test;
+
+/**
+ *@ClassName Son
+ *@Description TODO
+ *@Author lifay
+ *@Date 2023/1/10 21:12
+ **/
+
+/**
+ * 子类定义了父类
+ */
+class Son extends Base<DataClass> {
+
+    public Son(String json) {
+        super(json);
+    }
+}
